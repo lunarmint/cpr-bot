0ed79868a1317eee69201a817435ce4a58598d02
lunarmint <developer@chocomint.dev>
2022-10-04 10:41:14 -0400

    Added database initialization script

---
+++ scripts/mongo-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize mongodb user auth credentials.
+
+docker exec -it cpr-mongo bash -c 'mongosh --port "$MONGO_PORT" -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = "$MONGO_INITDB_ROOT_USERNAME";
+    var rootPassword = "$MONGO_INITDB_ROOT_PASSWORD";
+    var admin = db.getSiblingDB("admin");
+    admin.auth(rootUser, rootPassword);
+    db.createUser({user: "$MONGO_USERNAME", pwd: "$MONGO_PASSWORD", roles: [{role: "readWrite", db: "$MONGO_INITDB_DATABASE"}]});
+EOF'
