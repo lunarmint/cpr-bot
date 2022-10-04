9332936c8c215f72adc4cabbf3942ab7af4e0c98
lunarmint <developer@chocomint.dev>
2022-10-04 12:13:18 -0400

    Automatically exit on failure

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 printf "[1] Stopping cpr and mongo...\n"
 docker stop cpr mongo
 
@@ -10,8 +12,8 @@ printf "\n[3] Pruning any dangling Docker images and volumes...\n"
 docker image prune -f
 docker volume prune -f
 
-printf "\n[4] Pulling the latest Docker image...\n"
+printf "\n[4] Pulling Docker image from ghcr.io/lunarmint/cpr:latest...\n"
 docker-compose pull
 
-printf "\n[5] Starting the updated instance...\n"
+printf "\n[5] Starting the containers...\n"
 docker-compose up -d
