3f9339e737f2d41fee8110ce2edf84b3e92f4bed
lunarmint <developer@chocomint.dev>
2022-10-09 21:14:25 -0400

    Added back on_ready logging

--- modules/bot.py
+++ modules/bot.py
@@ -7,7 +7,6 @@ import discord
 from discord.ext import commands
 
 import __init__  # noqa
-import database
 from config import config
 
 bot = commands.Bot(
@@ -24,15 +23,22 @@ bot = commands.Bot(
 log = logging.getLogger(__name__)
 
 
-async def main():
+@bot.event
+async def on_ready() -> None:
+    """
+    Called when the client is done preparing the data received from Discord.
+    """
+    guilds = [guild.id for guild in bot.guilds]
+    log.info(f"Logged in as: {bot.user}")
+    log.info(f"Currently in {len(guilds)} guilds: {guilds}")
+
+
+async def main() -> None:
     for cog in glob.iglob(os.path.join("cogs", "**", "[!^_]*.py"), root_dir="modules", recursive=True):
         await bot.load_extension(cog.replace("/", ".").replace("\\", ".").replace(".py", ""))
 
     await bot.start(config["bot"]["token"])
-    log.info(f"Logged in as: {bot.user}")
 
-    guilds = [guild.id for guild in bot.guilds]
-    log.info(f"Currently in {len(guilds)} guilds: {guilds}")
 
 if __name__ == "__main__":
     asyncio.run(main())
