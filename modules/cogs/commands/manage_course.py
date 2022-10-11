3ec8e018fe3a9ff5f2cbec76ce7c4fcaa42eabfc
lunarmint <developer@chocomint.dev>
2022-10-11 15:26:50 -0400

    Updated embed formatting

--- modules/cogs/commands/manage_course.py
+++ modules/cogs/commands/manage_course.py
@@ -21,17 +21,25 @@ class ManageCourseCog(commands.GroupCog, group_name="course"):
     @app_commands.checks.has_role(config["roles"]["professor"])
     async def manage_course(self, interaction: discord.Interaction) -> None:
         embed = embeds.make_embed(
+            ctx=interaction,
+            author=True,
+            color=discord.Color.blurple(),
+            thumbnail_url="https://i.imgur.com/NBaYHQG.png",
             title="Manage Course",
             description="You can manage your courses using this interface.",
             footer="Use the buttons below to start.",
-            color=discord.Color.blurple(),
         )
         await interaction.response.send_message(embed=embed, view=ManageCourseButtons(), ephemeral=True)
 
     @manage_course.error
-    async def manage_course_error(self, interaction: discord.Interaction, error: discord.HTTPException):
+    async def manage_course_error(self, interaction: discord.Interaction, error: discord.HTTPException) -> None:
         if isinstance(error, discord.app_commands.errors.MissingRole):
             embed = embeds.make_embed(
+                ctx=interaction,
+                author=True,
+                color=discord.Color.red(),
+                thumbnail_url="https://i.imgur.com/boVVFnQ.png",
+                title="Error",
                 description=f"Role <@&{error.missing_role}> is required to use this command.",
             )
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -45,6 +53,28 @@ class ManageCourseButtons(discord.ui.View):
         label="Create Course", style=discord.ButtonStyle.green, custom_id="create_course_button", emoji="📖"
     )
     async def create_course_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        collection = database.Database().get_collection("courses")
+        query = {"guild_id": interaction.guild.id}
+        result = collection.find_one(query)
+        if result:
+            embed = embeds.make_embed(
+                ctx=interaction,
+                author=True,
+                color=discord.Color.red(),
+                thumbnail_url="https://i.imgur.com/boVVFnQ.png",
+                title="Failed to create course",
+                description="This server is already being associated with the following course:",
+                fields=[
+                    {"name": "Course Name:", "value": result["course_name"], "inline": False},
+                    {"name": "Course Abbreviation:", "value": result["course_abbreviation"], "inline": False},
+                    {"name": "Course Section:", "value": result["course_section"], "inline": False},
+                    {"name": "Semester:", "value": result["semester"], "inline": False},
+                    {"name": "CRN:", "value": result["crn"], "inline": False},
+                ],
+                footer="Use the 'Edit Course' button if you wish to update the course information.",
+            )
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+
         await interaction.response.send_modal(CreateCourseModal())
         await interaction.edit_original_response(view=None)
 
@@ -148,23 +178,29 @@ class CreateCourseModal(discord.ui.Modal, title="Create Course"):
         collection.insert_one(course_document)
 
         embed = embeds.make_embed(
-            title="Course Created",
+            ctx=interaction,
+            author=True,
+            color=discord.Color.green(),
+            thumbnail_url="https://i.imgur.com/W7VJssL.png",
+            title="Course created",
             description="Successfully created a new course with the following information:",
             fields=[
-                {"name": "Course Name:", "value": self.children[0].value, "inline": True},
-                {"name": "Course Abbreviation:", "value": self.children[1].value, "inline": True},
-                {"name": "Course Section:", "value": self.children[2].value, "inline": True},
-                {"name": "Semester:", "value": self.children[3].value, "inline": True},
-                {"name": "CRN:", "value": self.children[4].value, "inline": True},
+                {"name": "Course Name:", "value": self.children[0].value, "inline": False},
+                {"name": "Course Abbreviation:", "value": self.children[1].value, "inline": False},
+                {"name": "Course Section:", "value": self.children[2].value, "inline": False},
+                {"name": "Semester:", "value": self.children[3].value, "inline": False},
+                {"name": "CRN:", "value": self.children[4].value, "inline": False},
             ],
-            color=discord.Color.green(),
         )
+
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
         embed = embeds.make_embed(
-            description="Oops! Something went wrong. Please try again!",
             color=discord.Color.red(),
+            thumbnail_url="https://i.imgur.com/M1WQDzo.png",
+            title="Error",
+            description="Oops! Something went wrong. Please try again later!",
         )
         await interaction.response.send_message(embed=embed, ephemeral=True)
         log.error(error)
