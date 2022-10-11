662c585916eb3feb4ba0a44e2cfafaedf3c50578
lunarmint <developer@chocomint.dev>
2022-10-11 12:29:03 -0400

    Added docstrings and commands are now global

--- modules/cogs/commands/admin.py
+++ modules/cogs/commands/admin.py
@@ -4,7 +4,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from modules import config
 from modules.utils import embeds
 
 log = logging.getLogger(__name__)
@@ -16,6 +15,9 @@ class AdminCog(commands.GroupCog, group_name="sync"):
 
     @app_commands.command(name="global", description="Sync commands globally.")
     async def sync_global(self, interaction: discord.Interaction) -> None:
+        """
+        Does not sync all commands globally, just the ones registered as global.
+        """
         synced = await self.bot.tree.sync()
         embed = embeds.make_embed(
             description=f"Synced {len(synced)} commands globally.",
@@ -25,6 +27,9 @@ class AdminCog(commands.GroupCog, group_name="sync"):
 
     @app_commands.command(name="guild", description="Sync commands in the current guild.")
     async def sync_guild(self, interaction: discord.Interaction) -> None:
+        """
+        Does not sync all of your commands to that guild, just the ones registered to that guild.
+        """
         synced = await self.bot.tree.sync(guild=interaction.guild)
         embed = embeds.make_embed(
             description=f"Synced {len(synced)} commands to the current guild.",
@@ -34,6 +39,10 @@ class AdminCog(commands.GroupCog, group_name="sync"):
 
     @app_commands.command(name="copy", description="Copies all global app commands to current guild and syncs.")
     async def sync_global_to_guild(self, interaction: discord.Interaction) -> None:
+        """
+        This will copy the global list of commands in the tree into the list of commands for the specified guild.
+        This is not permanent between bot restarts, and it doesn't impact the state of the commands (you still have to sync).
+        """
         self.bot.tree.copy_global_to(guild=interaction.guild)
         synced = await self.bot.tree.sync(guild=interaction.guild)
         embed = embeds.make_embed(
@@ -54,7 +63,7 @@ class AdminCog(commands.GroupCog, group_name="sync"):
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(AdminCog(bot), guild=discord.Object(id=config["guild_id"]))
+    await bot.add_cog(AdminCog(bot))
     log.info("Command loaded: sync global")
     log.info("Command loaded: sync guild")
     log.info("Command loaded: sync copy")
