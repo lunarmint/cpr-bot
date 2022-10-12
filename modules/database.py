0ad4a8106c43abce9a0a2a80ab961f17ee0ebbef
lunarmint <developer@chocomint.dev>
2022-10-12 01:24:25 -0400

    Quote username and password according to RFC 3986

--- modules/database.py
+++ modules/database.py
@@ -1,4 +1,5 @@
 import logging
+import urllib.parse
 from typing import Any, Mapping
 
 from pymongo import MongoClient
@@ -11,8 +12,9 @@ log = logging.getLogger(__name__)
 
 class Database:
     def __init__(self) -> None:
-        self.username = config["database"]["username"]
-        self.password = config["database"]["password"]
+        # Username and password must be escaped according to RFC 3986, use urllib.parse.quote_plus.
+        self.username = urllib.parse.quote_plus(config["database"]["username"])
+        self.password = urllib.parse.quote_plus(config["database"]["password"])
         self.hostname = config["database"]["hostname"]
         self.port = config["database"]["port"]
         self.database_name = config["database"]["database_name"]
@@ -21,7 +23,7 @@ class Database:
             log.error("One or more database connection variables are missing.")
             raise SystemExit
 
-        self.url = f"mongodb://{self.username}:{self.password}@{self.hostname}:{self.port}/{self.database_name}"
+        self.url = f"mongodb://{self.username}:{self.password}@{self.hostname}:{self.port}/?authSource={self.database_name}"
         self.client = MongoClient(self.url)
 
     def get_collection(self, collection: str) -> Collection[Mapping[str, Any] | Any]:
